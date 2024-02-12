0ad9031e6dd11a66640a5e2c8f41149fe5f89e98
Tomas Zagorskis <tomzagos@gmail.com>
2024-02-12 08:51:58 +0200

    feat: add docker image

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	server: {
+		host: true,
+		port: 5173,
+		watch: {
+			usePolling: true,
+			ignored: ['**/node_modules/**', '**/dist/**'],
+		},
+	},
 });
